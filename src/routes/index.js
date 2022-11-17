e9909cb266d323d6337030338ccb06048ceff605
Eziogor Omojiro Clever <clever@flipfam.app>
2022-11-17 17:12:44 +0100

    text update

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(  'Express' );
+  res.send(  'TALENT PLUS' );
 });
 
 module.exports = router;
